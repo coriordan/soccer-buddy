fe0755c9e946525fa61096ad2826c7e985ea8463
Cathal O’Riordan <cathal@madein75.com>
2019-02-08 15:11:30 +0000

    adding model class to capture a Match

---
+++ app/src/main/java/com/example/soccerbuddy/model/Match.java
@@ -0,0 +1,107 @@
+package com.example.soccerbuddy.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import java.util.Date;
+
+public class Match implements Parcelable {
+
+
+    public Match() {}
+
+    String title;
+    String description;
+    int playersRequired;
+    Date fixtureDate;
+    SkillLevel skillLevel;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPlayersRequired() {
+        return playersRequired;
+    }
+
+    public void setPlayersRequired(int playersRequired) {
+        this.playersRequired = playersRequired;
+    }
+
+    public Date getFixtureDate() {
+        return fixtureDate;
+    }
+
+    public void setFixtureDate(Date fixtureDate) {
+        this.fixtureDate = fixtureDate;
+    }
+
+    public SkillLevel getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(SkillLevel skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    protected Match(final Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        playersRequired = in.readInt();
+
+        final long time = in.readLong();
+        fixtureDate = time != -1 ? new Date(time) : null;
+
+        final int skillLevelOrd = in.readInt();
+        skillLevel = skillLevelOrd != -1
+                ? SkillLevel.values()[skillLevelOrd]
+                : null;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeInt(playersRequired);
+        dest.writeLong(fixtureDate != null ? fixtureDate.getTime() : -1);
+        dest.writeInt(skillLevel != null ? skillLevel.ordinal() : -1);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Match> CREATOR = new
+            Creator<Match>() {
+                @Override
+                public Match createFromParcel(Parcel in) {
+                    return new Match(in);
+                }
+                @Override
+                public Match[] newArray(int size) {
+                    return new Match[size];
+                }
+            };
+
+    public boolean isValid() {
+        return !TextUtils.isEmpty(title)
+                && !TextUtils.isEmpty(description)
+                && playersRequired > 0
+                && fixtureDate != null
+                && skillLevel != null;
+    }
+}
