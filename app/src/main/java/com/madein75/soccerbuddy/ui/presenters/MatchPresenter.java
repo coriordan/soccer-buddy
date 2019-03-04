e842392ebbbc6befb0e586a5015e55b8bcbe7984
Cathal O’Riordan <cathal@madein75.com>
2019-03-04 12:40:03 +0000

    simplify a little, no need to pass context

--- app/src/main/java/com/madein75/soccerbuddy/ui/presenters/MatchPresenter.java
+++ app/src/main/java/com/madein75/soccerbuddy/ui/presenters/MatchPresenter.java
@@ -9,12 +9,10 @@ import java.util.Date;
 
 public class MatchPresenter {
 
-    private final Context context;
     private DateFormat dateFormat;
     private DateFormat timeFormat;
 
-
-    public MatchPresenter(final Context context) { this.context = context; }
+    public MatchPresenter() { }
 
     public String formatDate(final Date date) {
         if (dateFormat == null) {
