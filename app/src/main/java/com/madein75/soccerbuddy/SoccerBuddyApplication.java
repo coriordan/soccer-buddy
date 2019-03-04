e75ccb9542ec7b9924c0f6c823b504f6077f2a40
Cathal O’Riordan <cathal@madein75.com>
2019-03-04 12:36:33 +0000

    removing Room related database stuff

--- app/src/main/java/com/madein75/soccerbuddy/SoccerBuddyApplication.java
+++ app/src/main/java/com/madein75/soccerbuddy/SoccerBuddyApplication.java
@@ -2,19 +2,11 @@ package com.madein75.soccerbuddy;
 
 import android.app.Application;
 
-import com.madein75.soccerbuddy.db.SoccerBuddyRepository;
-
 public class SoccerBuddyApplication extends Application {
 
-    private static SoccerBuddyRepository repository;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        repository = new SoccerBuddyRepository(this);
-    }
-
-    public static SoccerBuddyRepository getSoccerBuddyRepository() {
-        return repository;
     }
 }
