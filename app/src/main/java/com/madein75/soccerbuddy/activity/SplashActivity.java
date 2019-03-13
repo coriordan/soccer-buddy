7dddba2f3874be2cd751ca336545934e3c4e473d
Cathal O’Riordan <cathal@madein75.com>
2019-03-13 11:40:37 +0000

    Splash activity launches Main activity after set period of time

--- app/src/main/java/com/madein75/soccerbuddy/activity/SplashActivity.java
+++ app/src/main/java/com/madein75/soccerbuddy/activity/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.madein75.soccerbuddy.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,32 @@ import com.madein75.soccerbuddy.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
+    private boolean isBackButtonPressed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                finish();
+
+                if (!isBackButtonPressed) {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onBackPressed() {
+        isBackButtonPressed = true;
+        super.onBackPressed();
     }
 }
